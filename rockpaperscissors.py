3d200e9a8360c4b38d6e1d055e49902bb5e6dc58
Andrew McCartney <mccartneyac@gmail.com>
2023-09-27 08:42:10 -0400

    Update rockpaperscissors.py
    add TW's version

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -33,3 +33,36 @@ while start.lower() == "yes":
     game = input("do you want to play again?\n")
     if game.lower() == "no":
         start = "no"
+
+
+# TW's Version (with chatGPT)
+
+import random
+def play_game(): 
+    user = input("Select one:\n   rock, paper, scissors\n").lower()
+    my_list = ["rock", "paper", "scissors"]
+    computer = random.choice(my_list)
+
+    if user in my_list:
+        winner_dict = {
+            # mccartney note: probably a dictionary is too much here and a simple list-of-lists would have sufficed. 
+            ("rock", "scissors"): "user",
+            ("scissors", "paper"): "user",
+            ("paper", "rock"): "user",
+        }
+
+        if user == computer:
+            print("It's a tie!")
+        elif (user, computer) in winner_dict:
+            print("Congratulations! You win!")
+        else:
+            print("You Lose! Better luck next time")
+    else:
+        print("Invalid input. Please select from 'rock', 'paper', or 'scissors'.")
+
+    print("The computer chose", computer)
+
+start = input("Would you like to play a game? (yes/no)\n")
+while start.lower() == "yes":
+    play_game()
+    start = input("Do you want to play again? (yes/no)\n")
