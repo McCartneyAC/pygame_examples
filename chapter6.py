6330d7ecc743f7d9fb70ecdaf0776c511093aa77
Andrew McCartney <mccartneyac@gmail.com>
2024-01-24 11:58:26 -0500

    Create chapter6.py

---
+++ chapter6.py
@@ -0,0 +1,178 @@
+print("puzzle 1\n")
+for i in range(10):
+  print("*", end = "")
+print("\n")
+
+
+
+
+print("Puzzle 2")
+for row in range(5): 
+  print("*", end=" ")
+print()
+for row in range(10):
+  print("*",end=" ")
+print()
+for row in range(20): 
+  print("*", end=" ")
+print()
+
+
+
+
+
+
+
+print("Puzzle 3")
+
+for row in range(10):
+  for column in range(10):
+    print("*", end = " ")
+  print()
+
+
+
+
+
+
+
+
+print("\n Puzzle 4")
+for row in range(10):
+  for column in range(5):
+    print("*", end = " ")
+  print()
+
+
+
+
+
+
+
+print("\n Puzzle 5")
+for row in range(5):
+  for column in range(20):
+    print("*", end = " ")
+  print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("\n puzzle 6")
+for row in range(10):
+  for column in range(10):
+    print(column, end = " ")
+  print()
+
+
+
+
+
+
+
+
+
+print("\n puzzle 7")
+for row in range(10):
+  for column in range(10):
+    print(row, end = " ")
+  print()
+
+print("\n puzzle 8")
+for row in range(10):
+  for column in range(row+1):
+    print(column, end = " ")
+  print()
+
+print("\n puzzle 9")
+for row in range(10):
+  for j in range(row):
+    print (" ",end=" ")
+  for j in range(10-row):
+    print (j,end=" ")
+  print()
+
+print("\n puzzle 10")
+for row in range(1, 10):
+  for column in range(1, 10):
+    if len(str(row * column)) == 1: 
+      print(" ", row* column, end = " ")
+    else: 
+      print("", row*column, end = " ")
+  print()
+
+
+
+
+
+#problem # 11
+for row in range(1, 10):
+   for column in range(10,row, -1):
+     print(" ", end = " ")
+   for column in range(0,row):
+     print(column+ 1, end = " ")
+   for column in range(row-1, 0, -1):
+      print(column, end = " ")
+   print()
+print(" ")
+
+
+
+
+# problem 12
+for row in range(1, 10):
+  for column in range(10,row, -1):
+    print(" ", end = " ")
+  for column in range(0,row):
+    print(column+ 1, end = " ")
+  for column in range(row-1, 0, -1):
+     print(column, end = " ")
+  print()
+for row in range(9, 1, -1):
+  print("    ", end = "")
+  for column in range(row, 9, 1):
+    print(" ", end = " ")
+  for column in range(1, row):
+    print(column, end = " ")
+  for column in range(row-2, 0, -1):
+    print(column, end = " ")
+  print()
+
+
+
+
+print("let's do the lab")
+print("problem 1")
+start = 10
+for row in range(10):
+  for column in range(row):
+    print(start, end = " ")
+    start +=1
+  print()
+
+
+print("***problem 2****")
+n = input("how big is your box? \n")
+n = int(n)
+for row in range(0, n):
+  print("o", end = "")
+print()
+for row in range(0, n-2):
+  print("o", end = "")
+  for column in range(0, n-2):
+    print(" ", end = "")
+  print("o")
+for row in range(0,n):
+  print("o", end="")
+
+
